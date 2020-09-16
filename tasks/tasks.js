b939068ec3cfffd30fc702232bac29dedce2f39b
strannic123 <manager350@mail.ru>
2020-09-16 16:40:37 +0300

    add задача2

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -13,53 +13,87 @@
 
 
 //Реализуем  функцию getOwnDivisors - массив делителей
-function getOwnDivisors(num) {
-    let res = [];
-    for (let i = 1; i < num; i++) {
-        if (num % i == 0) {
-            res.push(i)
-        }
-    }
-    return res
-}
-
-
+// function getOwnDivisors(num) {
+//     let res = [];
+//     for (let i = 1; i < num; i++) {
+//         if (num % i == 0) {
+//             res.push(i)
+//         }
+//     }
+//     return res
+// }
 
 //Реализуем функцию getSum. Которая вернет сумму делителей
 
-function getSum(arr) {
-    let res = 0;
-    for (let elem of arr) {
-        res += elem
-    }
-    return res;
-}
+// function getSum(arr) {
+//     let res = 0;
+//     for (let elem of arr) {
+//         res += elem
+//     }
+//     return res;
+// }
 
 //Реализуем функцию сравнения чисел
-function isFreindly(num1, num2) {
-    let sum1 = (getSum(getOwnDivisors(num1))),
-        sum2 = (getSum(getOwnDivisors(num2)));
-
-    return sum1 === num2 && sum2 === num1
-}
+// function isFreindly(num1, num2) {
+//     let sum1 = (getSum(getOwnDivisors(num1))),
+//         sum2 = (getSum(getOwnDivisors(num2)));
+//
+//     return sum1 === num2 && sum2 === num1
+// }
 
 // Реализуем поиск чисел дружественных делителей в нужном диапазоне(при запуске функции на больших числах - более 2000,
 // значительная нагрузка на процессор - длительное ожидание результата)
-function getFreindly(min, max) {
-    let res = [];
-    for (let i = min; i <= max; i++) {
-        for (let j = i + 1; j <= max; j++) {
-            if (isFreindly(i, j)) {
-                console.log(i, j)
-                res.push([i, j])
-            }
-        }
-    }
-    return res;
-}
-
-
-console.log(getFreindly(1, 1000))
+// function getFreindly(min, max) {
+//     let res = [];
+//     for (let i = min; i <= max; i++) {
+//         for (let j = i + 1; j <= max; j++) {
+//             if (isFreindly(i, j)) {
+//                 console.log(i, j)
+//                 res.push([i, j])
+//             }
+//         }
+//     }
+//     return res;
+// }
+//
+//
+// console.log(getFreindly(1, 1000))
 
 
 // console.log(isFreindly(220, 284))
+
+
+//задача 2
+
+//Совершенное число - целое число, равное сумме всех своих собственных делителей (то есть всех положительных делителей, отличных от самого числа). Сделайте функцию которая будет находить совершенные числа в заданном диапазоне. Проверьте работу функции в промежутке от 1 до 1000.
+
+// function getOwnDivisor(num) {
+// 	let res = [];
+// 	for (let i = 1; i < num; i++){
+// 		if (num % i == 0){
+// 			res.push(i)
+// 		}
+// 	}
+// 	return res;
+// }
+
+// function getSum(arr) {
+// 	let res = 0;
+// 	for (let elem of arr){
+// 		res += elem;
+// 	}
+// 	return res
+// }
+
+// function getPerfect(min, max) {
+// 	let resPerfectNumber = []
+// 	for (let i = min; i < max; i++){
+// 		let findNum = getSum(getOwnDivisor(i))
+// 		if (findNum === i){
+// 			resPerfectNumber.push(i)
+// 		}
+// 	}
+// 	return resPerfectNumber;
+// }
+//
+// console.log(getPerfect(1, 100000))
