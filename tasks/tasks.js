d9cede47223a86a140f8fa99b06bc8ae81bbfa81
strannic123 <manager350@mail.ru>
2020-09-22 21:24:28 +0300

    add задача 28

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -828,3 +828,37 @@ function primitiveArr(arr) {
 
 primitiveArr(arr)
 console.log(counter)
+
+
+//задача 28
+
+//Пересечение произвольного количества массивов
+
+function getInt(...arrs) {
+	let result = [];
+
+	let arr0 = arrs.shift();
+
+	for (let elem of arr0) {
+		if (inArrays(elem, arrs)) {
+			result.push(elem);
+		}
+	}
+	return result;
+}
+
+function inArrays(elem, arrs) {
+	for (let arr of arrs){
+		if(!inArray(elem, arr)){
+			return false;
+		}
+	}
+	return true;
+}
+
+function inArray(elem, arr){
+	return arr.indexOf(elem) !== -1;
+}
+
+let result = getInt([1, 2, 3], [2, 3, 4], [4, 3, 2]);
+console.log(result);
